00faf9c73dcfb194207d8462859a1a22ffd007c8
Kinga <kingakwiatkowskaszkola@gmail.com>
2025-03-21 11:56:53 +0100

    wip zestaw4b

--- Zestaw_4b/src/main.rs
+++ Zestaw_4b/src/main.rs
@@ -1,15 +1,32 @@
-use std::ops::Add;
-
-fn szyfruj(napis: &str, klucz: u32) -> String {
+fn dodaj_pismnie(napis1: &str, napis2: &str) -> String {
     let mut wynik = String::new();
+    let mut pozyczka = 0;
 
-    for i in napis.chars().rev() {
-        wynik.push(i);
+    let liczby1: Vec<char> = napis1.chars().collect();
+    let liczby2: Vec<char> = napis2.chars().collect();
+
+    let mut max_dlugosc;
+    if liczby1.len() >= liczby2.len() {
+        max_dlugosc = liczby1.len();
+    } else {
+        max_dlugosc = liczby2.len();
+    }
+
+    for i in 0..max_dlugosc {
+        let cyfra1
     }
 
     wynik
 }
 
 fn main() {
-    println!("{}", szyfruj("Aladyn", 2))
+    println!("{}", dodaj_pismnie("1", "3")); // 4
+    println!("{}", dodaj_pismnie("8", "3")); // 11
+    println!("{}", dodaj_pismnie("10", "23")); // 33
+    println!("{}", dodaj_pismnie("1", "0")); // 1
+    println!("{}", dodaj_pismnie("11", "00")); // 11
+    println!("{}", dodaj_pismnie("131", "9900")); // 10031
+    println!("{}", dodaj_pismnie("998", "7")); // 1005
+    println!("{}", dodaj_pismnie("24872947", "294729478")); // 319602425
+    println!("{}", dodaj_pismnie("5924729874298749827418582", "6782893629472094209740298")); // 12707623503770844037158880
 }
--- Zestaw_4b/target/debug/deps/Zestaw_4b.d
+++ Zestaw_4b/target/debug/deps/Zestaw_4b.d
@@ -1,5 +1,5 @@
-C:\Users\kinga\RustroverProjects\Rust_PF1\Zestaw_4b\target\debug\deps\Zestaw_4b.exe: src\main.rs
+D:\szkola\studia\4 sem\Bezpieczne programowanie systemowe w języku Rust\Repo\Zestaw_4b\target\debug\deps\Zestaw_4b.exe: src\main.rs
 
-C:\Users\kinga\RustroverProjects\Rust_PF1\Zestaw_4b\target\debug\deps\Zestaw_4b.d: src\main.rs
+D:\szkola\studia\4 sem\Bezpieczne programowanie systemowe w języku Rust\Repo\Zestaw_4b\target\debug\deps\Zestaw_4b.d: src\main.rs
 
 src\main.rs:
